69efc8a3d7c6c4a2da2a26dc3138ca3548de4e67
iwgx <ilhamwahabi@iwgxs-MacBook-Pro.local>
2023-07-10 00:22:48 +0700

    Adjust homepage typogh

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -42,13 +42,17 @@ export const meta: V2_MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex flex-col justify-center items-center p-16">
-      <h1 className="text-center text-3xl md:text-4xl">
+    <div className="flex flex-col justify-center items-center p-6 lg:p-16">
+      <h1 className="text-center text-2xl md:text-4xl">
         Selamat datang di{" "}
         <span className="text-[#F6983F]">ilhamwahabi.com</span>
       </h1>
-      <p className="mt-16 text-2xl">Masih bingung mau diisi apa</p>
-      <p className="mt-4 text-2xl">Silakan liat-liat yang lain dulu aja ya</p>
+      <p className="mt-16 text-base md:text-2xl text-center">
+        Masih bingung mau diisi apa
+      </p>
+      <p className="mt-4 text-base md:text-2xl text-center">
+        Silakan liat-liat yang lain dulu aja ya
+      </p>
     </div>
   );
 }
