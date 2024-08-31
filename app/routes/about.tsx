8b03d3a2c5e61e400c93af7ec08443fef1adc9a8
ilhamwahabi <wahabiputra@gmail.com>
2024-08-31 23:33:22 +0700

    Update about

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -17,18 +17,24 @@ export default function About() {
         Hi, <span className="text-[#F6983F]">Ilham Wahabi</span> here 👋
       </h1>
       <div className="mt-8 lg:mt-12 text-left space-y-6 md:space-y-8 text-base md:text-xl flex flex-col items-start w-full">
-        <p>I'm a frontend developer based in Bandung, Indonesia.</p>
+        <p>
+          I'm a frontend developer based in <s>Bandung</s> Jakarta, Indonesia.
+        </p>
         <p>
           Graduated from Institut Teknologi Bandung with a major in Informatics
           (Computer Science).
         </p>
         <p>
-          I have more than 4 years of experience working as a Software Engineer
-          in various companies in Indonesia and Singapore.
+          I have over 5 years of experience as a Software Engineer, working
+          across various industries in companies based in Indonesia and
+          Singapore.
         </p>
         <p>Interested in SEO, accessibility, and performance optimization.</p>
         <p>Besides coding, I enjoy reading books and playing Badminton.</p>
-        <p>If you have an interesting opportunity, please let me know 👇</p>
+        <p>
+          If you have an interesting opportunity, please let me know through one
+          of the contacts below 👇
+        </p>
       </div>
     </div>
   );
