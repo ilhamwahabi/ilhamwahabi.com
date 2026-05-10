78bd19e1a391c21861ce2f28671df5f60628041e
ilhamwahabi <wahabiputra@gmail.com>
2026-05-10 14:37:17 +0700

    update home

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,17 @@
-import { Link, createFileRoute } from '@tanstack/react-router'
-import { getSeoHead } from '#/lib/seo'
+import { Link, createFileRoute } from "@tanstack/react-router";
+import { getSeoHead } from "#/lib/seo";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   head: () => ({
     ...getSeoHead({
-      title: '',
-      description: 'UI Engineer based in Jakarta, Indonesia',
-      url: '',
-      keywords: '',
+      title: "",
+      description: "UI Engineer based in Jakarta, Indonesia",
+      url: "",
+      keywords: "",
     }),
   }),
   component: Index,
-})
+});
 
 function Index() {
   return (
@@ -20,7 +20,7 @@ function Index() {
         Hi, <span className="text-sky-600">Ilham</span> here 👋
       </h1>
       <p className="mt-8 text-base md:mt-16 md:text-2xl">
-        I'm a software engineer currently working at{' '}
+        I'm a software engineer currently working at{" "}
         <a href="https://www.traveloka.com" target="_blank" rel="noreferrer">
           <span className="text-sky-600">Traveloka</span>
           <img
@@ -31,9 +31,12 @@ function Index() {
         </a>
       </p>
       <p className="mt-8 text-base md:text-2xl">
-        I've several years of experience on building products that generate billions in revenue, serve large enterprises, and operate across multiple countries.      </p>
+        I've helped building products that generate billions in revenue, serve
+        large enterprises, and being used by users across multiple
+        countries.{" "}
+      </p>
       <p className="mt-8 text-base md:text-2xl">
-        You can find out more about me{' '}
+        You can find out more about me{" "}
         <Link className="underline underline-offset-4" to="/about">
           here
         </Link>
@@ -41,12 +44,12 @@ function Index() {
       </p>
       <p className="mt-8 text-base md:text-2xl">
         I also <span className="text-sky-600">write blog</span> on technology,
-        opinions, and my life journey ✍️ which you can{' '}
+        opinions, and my life journey ✍️ which you can{" "}
         <Link className="underline underline-offset-4" to="/blog">
           read here
         </Link>
         .
       </p>
     </div>
-  )
+  );
 }
